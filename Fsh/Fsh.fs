aac7f18cb37d9bc1e0aa3bf9dfec146ae9d93d8c
Alexander V. Diemand <codieplusplus@apax.net>
2017-07-31 16:26:04 +0200

    init

---
+++ Fsh/Fsh.fs
@@ -0,0 +1,16 @@
+﻿namespace Fsh
+
+open System
+open Xwt
+
+module FshButton =
+
+    let create (lbl : string) =
+        let b = new Xwt.Button(lbl)
+        b
+
+    let createWithHandler (lbl : string) (hdlr : Xwt.Button -> EventArgs -> unit) =
+        let b = new Xwt.Button(lbl)
+        b.Clicked.Add(hdlr b)
+        b
+
---
+++ Fsh/Fsh.fsi
@@ -0,0 +1,9 @@
+﻿namespace Fsh
+
+open System
+open Xwt
+
+module FshButton =
+
+    val create : string -> Xwt.Button
+    val createWithHandler : string -> (Xwt.Button -> EventArgs -> unit) -> Xwt.Button
